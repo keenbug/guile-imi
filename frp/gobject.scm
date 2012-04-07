a0d326f8a758e53ebe7d0e7935d9f7cbe6b59ad6
Daniel Krueger <keenbug@gmail.com>
2012-04-07 20:59:40 +0200

    removed info messages from (frp gobject)

--- frp/gobject.scm
+++ frp/gobject.scm
@@ -10,38 +10,32 @@
 (define (gobject-event gobj signal-name)
   (make-event
     (lambda (frp-event)
-      (format #t "using event ~s~%" signal-name)
       (connect gobj signal-name
         (lambda (widget gsignal-event)
           (event-trigger frp-event gsignal-event)
           #t)))
     (lambda (handler-id)
-      (format #t "unusing event ~s~%" signal-name)
       (disconnect gobj handler-id))))
 
 (define (gobject-callback gobj signal-name)
   (make-event
     (lambda (frp-event)
-      (format #t "using callback ~s~%" signal-name)
       (connect gobj signal-name
         (lambda (widget)
           (event-trigger frp-event #t)
           #t)))
     (lambda (handler-id)
-      (format #t "unusing callback ~s~%" signal-name)
       (disconnect gobj handler-id))))
 
 
 (define (gobject-event* gobj signal-name)
   (make-event
     (lambda (frp-event)
-      (format #t "using event* ~s~%" signal-name)
       (connect gobj signal-name
         (lambda args
           (event-trigger frp-event args)
           #t)))
     (lambda (handler-id)
-      (format #t "unusing event* ~s~%" signal-name)
       (disconnect gobj handler-id))))
 
 
@@ -49,13 +43,11 @@
 (define (msec-tick msecs)
   (make-event
     (lambda (frp-event)
-      (format #t "start ~s tick~%" msecs)
       (g-timeout-add msecs
         (lambda ()
           (event-trigger frp-event #t)
           #t)))
     (lambda (source-id)
-      (format #t "stop ~s tick (id ~s)~%" msecs source-id)
       (g-source-remove source-id))))
 
 
